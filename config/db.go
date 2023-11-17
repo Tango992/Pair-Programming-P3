a6524518f425a37574750d452f425d8b04a896ec
Tango992 <daniel.rahmanto@gmail.com>
2023-11-17 13:04:20 +0700

    dockerize

--- config/db.go
+++ config/db.go
@@ -10,7 +10,7 @@ import (
 )
 
 func ConnectDB() *mongo.Client {
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://127.0.0.1:27017/"))
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://host.docker.internal:27017/"))
 	if err != nil {
 		log.Fatal(err)
 	}
